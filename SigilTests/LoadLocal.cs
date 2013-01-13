4a251586b1c97dfafe6e1e74ab59467a6f5efa46
Kevin Montrose <kevin.montrose@stackoverflow.com>
2013-01-13 17:31:15 -0500

    More local tests

--- SigilTests/LoadLocal.cs
+++ SigilTests/LoadLocal.cs
@@ -27,5 +27,25 @@ namespace SigilTests
 
             Assert.AreEqual(3, del());
         }
+
+        [TestMethod]
+        public void Unused()
+        {
+            var e1 = Emit<Func<int>>.NewDynamicMethod("E1");
+
+            var foo = e1.CreateLocal<int>("foo");
+            e1.LoadConstant(3);
+            e1.Return();
+
+            try
+            {
+                var del = e1.CreateDelegate();
+                Assert.Fail("Shouldn't be able to create a delegate with unused locals");
+            }
+            catch (SigilException e)
+            {
+                Assert.AreEqual("Locals [foo] were declared but never used", e.Message);
+            }
+        }
     }
 }
