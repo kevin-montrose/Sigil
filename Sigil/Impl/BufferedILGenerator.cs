1e50fd2ba3e65d5ade6490ff566d7d38a3684d36
Kevin Montrose <kevin.montrose@stackoverflow.com>
2013-01-13 18:47:50 -0500

    Hacking up a buffered ILGenerator wrapper for *_S replacement

---
+++ Sigil/Impl/BufferedILGenerator.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sigil.Impl
+{
+    internal class BufferedILGenerator
+    {
+        public delegate Label DefineLabelDelegate(ILGenerator il);
+        public delegate LocalBuilder DeclareLocallDelegate(ILGenerator il);
+
+        public int Index { get { return Buffer.Count; } }
+
+        private List<Action<ILGenerator>> Buffer = new List<Action<ILGenerator>>();
+
+        private Type DelegateType;
+
+        public BufferedILGenerator(Type delegateType)
+        {
+            DelegateType = delegateType;
+        }
+
+        public void UnBuffer(ILGenerator il)
+        {
+            foreach (var x in Buffer)
+            {
+                x(il);
+            }
+        }
+
+        public int ByteDistance(int start, int stop)
+        {
+            var invoke = DelegateType.GetMethod("Invoke");
+            var returnType = invoke.ReturnType;
+            var parameterTypes = invoke.GetParameters().Select(s => s.ParameterType).ToArray();
+
+            var dynMethod = new DynamicMethod(Guid.NewGuid().ToString(), returnType, parameterTypes);
+            var il = dynMethod.GetILGenerator();
+
+            foreach (var x in Buffer.Skip(start).Take(stop - start))
+            {
+                x(il);
+            }
+
+            return il.ILOffset;
+        }
+
+        public void Emit(OpCode op)
+        {
+            Buffer.Add(il => il.Emit(op));
+        }
+
+        public void Emit(OpCode op, int i)
+        {
+            Buffer.Add(il => il.Emit(op, i));
+        }
+
+        public void Emit(OpCode op, long l)
+        {
+            Buffer.Add(il => il.Emit(op, l));
+        }
+
+        public void Emit(OpCode op, float f)
+        {
+            Buffer.Add(il => il.Emit(op, f));
+        }
+
+        public void Emit(OpCode op, double d)
+        {
+            Buffer.Add(il => il.Emit(op, d));
+        }
+
+        public void Emit(OpCode op, DefineLabelDelegate label)
+        {
+            Buffer.Add(il => il.Emit(op, label(il)));
+        }
+
+        public void Emit(OpCode op, DeclareLocallDelegate local)
+        {
+            Buffer.Add(il => il.Emit(op, local(il)));
+        }
+
+        public DefineLabelDelegate DefineLabel()
+        {
+            ILGenerator forIl = null;
+            Label? l = null;
+
+            return
+                il =>
+                {
+                    if(forIl != null && forIl != il)
+                    {
+                        l = null;
+                    }
+
+                    if (l != null) return l.Value;
+                    
+                    forIl = il;
+                    l = forIl.DefineLabel();
+
+                    return l.Value;
+                };
+        }
+
+        public void MarkLabel(DefineLabelDelegate label)
+        {
+            Buffer.Add(il => il.MarkLabel(label(il)));
+        }
+
+        public DeclareLocallDelegate DeclareLocal(Type type)
+        {
+            ILGenerator forIl = null;
+            LocalBuilder l = null;
+
+            return
+                il =>
+                {
+                    if(forIl != null && il != forIl)
+                    {
+                        l = null;
+                    }
+
+                    if (l != null) return l;
+
+                    forIl = il;
+                    l = forIl.DeclareLocal(type);
+
+                    return l;
+                };
+        }
+    }
+}
