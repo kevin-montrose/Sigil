a2d6ba1c4dc9cc4538884e31c0af65cda121a95a
Kevin Montrose <kevin.montrose@stackoverflow.com>
2013-01-19 15:15:03 -0500

    Duplicate

---
+++ Sigil/Emit.Duplicate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sigil
+{
+    public partial class Emit<DelegateType>
+    {
+        public void Duplicate()
+        {
+            var onStack = Stack.Top();
+
+            if (onStack == null)
+            {
+                throw new SigilException("Duplicate expects a value on the stack, but it was empty", Stack);
+            }
+
+            UpdateState(OpCodes.Dup, onStack[0]);
+        }
+    }
+}
