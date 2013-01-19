7be83d8d8eca96eb86fc4a503f3098f044b5fe14
Kevin Montrose <kevin.montrose@stackoverflow.com>
2013-01-19 00:31:45 -0500

    LoadArg

---
+++ Sigil/Emit.LoadArgument.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+using Sigil.Impl;
+
+namespace Sigil
+{
+    public partial class Emit<DelegateType>
+    {
+        public void LoadArgument(int index)
+        {
+            if (ParameterTypes.Length == 0)
+            {
+                throw new InvalidOperationException("Delegate of type " + typeof(DelegateType) + " takes no parameters");
+            }
+
+            if (index < 0 || index >= ParameterTypes.Length)
+            {
+                throw new ArgumentOutOfRangeException("index must be between 0 and " + (ParameterTypes.Length - 1) + ", inclusive");
+            }
+
+            switch (index)
+            {
+                case 0: UpdateState(OpCodes.Ldarg_0, TypeOnStack.Get(ParameterTypes[index])); return;
+                case 1: UpdateState(OpCodes.Ldarg_1, TypeOnStack.Get(ParameterTypes[index])); return;
+                case 2: UpdateState(OpCodes.Ldarg_2, TypeOnStack.Get(ParameterTypes[index])); return;
+                case 3: UpdateState(OpCodes.Ldarg_3, TypeOnStack.Get(ParameterTypes[index])); return;
+            }
+
+            if (index <= byte.MaxValue)
+            {
+                UpdateState(OpCodes.Ldarg_S, index, TypeOnStack.Get(ParameterTypes[index]));
+                return;
+            }
+
+            UpdateState(OpCodes.Ldarg, index, TypeOnStack.Get(ParameterTypes[index]));
+        }
+    }
+}
---
+++ SigilTests/LoadArguments.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Sigil;
+
+namespace SigilTests
+{
+    [TestClass]
+    public class LoadArguments
+    {
+        [TestMethod]
+        public void Simple()
+        {
+            var e1 = Emit<Func<int, string>>.NewDynamicMethod("E1");
+            e1.LoadArgument(0);
+            e1.Box<int>();
+            e1.CallVirtual(typeof(object).GetMethod("ToString"));
+            e1.Return();
+
+            var d1 = e1.CreateDelegate();
+
+            Assert.AreEqual("31415", d1(31415));
+        }
+    }
+}
