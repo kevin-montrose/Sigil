bc8cca331e299084cea542355db67c9e5f413480
Kevin Montrose <kevin.montrose@stackoverflow.com>
2013-01-19 15:51:50 -0500

    InitializeObject

---
+++ Sigil/Emit.InitializeBlock.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Sigil.Impl;
+using System.Reflection.Emit;
+
+namespace Sigil
+{
+    public partial class Emit<DelegateType>
+    {
+        public void InitializeBlock()
+        {
+            var onStack = Stack.Top(3);
+
+            if (onStack == null)
+            {
+                throw new SigilException("InitializeBlock expects three values to be on the stack", Stack);
+            }
+
+            var start = onStack[2];
+            var init = onStack[1];
+            var count = onStack[0];
+
+            if (!start.IsPointer && !start.IsReference && start != TypeOnStack.Get<NativeInt>())
+            {
+                throw new SigilException("InitializeBlock expects the start value to be a pointer, reference, or native int; found " + start, Stack);
+            }
+
+            if (init != TypeOnStack.Get<int>() && init != TypeOnStack.Get<NativeInt>())
+            {
+                throw new SigilException("InitBlock expects the initial value to be an int or native int; found " + init, Stack);
+            }
+
+            if (count != TypeOnStack.Get<int>())
+            {
+                throw new SigilException("InitBlock expects the count to be an int; found " + count, Stack);
+            }
+
+            UpdateState(OpCodes.Initblk, pop: 3);
+        }
+    }
+}
---
+++ Sigil/Emit.InitializeObject.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Sigil.Impl;
+using System.Reflection.Emit;
+
+namespace Sigil
+{
+    public partial class Emit<DelegateType>
+    {
+        public void InitializeObject<ValueType>()
+        {
+            InitializeObject(typeof(ValueType));
+        }
+
+        public void InitializeObject(Type valueType)
+        {
+            if (valueType == null)
+            {
+                throw new ArgumentNullException("valueType");
+            }
+
+            var onStack = Stack.Top();
+
+            if (onStack == null)
+            {
+                throw new SigilException("InitializeObject expects a value to be on the stack, but it was empty", Stack);
+            }
+
+            var obj = onStack[0];
+
+            if (obj != TypeOnStack.Get(valueType))
+            {
+                throw new SigilException("InitializeObject expected " + valueType + " to be on the stack; found " + obj, Stack);
+            }
+
+            UpdateState(OpCodes.Initobj, valueType, pop: 1);
+        }
+    }
+}
