374138b2382204f323a2359333e39e9319ad0728
Kevin Montrose <kevin.montrose@stackoverflow.com>
2013-01-19 13:32:13 -0500

    CastClass

---
+++ Sigil/Emit.CastClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+using Sigil.Impl;
+
+namespace Sigil
+{
+    public partial class Emit<DelegateType>
+    {
+        public void CastClass<ReferenceType>()
+        {
+            CastClass(typeof(ReferenceType));
+        }
+
+        public void CastClass(Type referenceType)
+        {
+            if (referenceType == null)
+            {
+                throw new ArgumentNullException("referenceType");
+            }
+
+            if (referenceType.IsValueType)
+            {
+                throw new SigilException("Can only cast to ReferenceTypes, found " + referenceType, Stack);
+            }
+
+            var top = Stack.Top();
+
+            if (top == null)
+            {
+                throw new SigilException("CastClass expects a value on the stack, but it was empty", Stack);
+            }
+
+            UpdateState(OpCodes.Castclass, TypeOnStack.Get(referenceType), pop: 1);
+        }
+    }
+}
