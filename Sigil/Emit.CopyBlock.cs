9bc680505c1df41e0fcf7a64adeda343592a0934
Kevin Montrose <kevin.montrose@stackoverflow.com>
2013-01-19 15:32:54 -0500

    CopyBlock

---
+++ Sigil/Emit.CopyBlock.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Sigil.Impl;
+using System.Reflection.Emit;
+
+namespace Sigil
+{
+    public partial class Emit<DelegateType>
+    {
+        public void CopyBlock()
+        {
+            var onStack = Stack.Top(3);
+
+            if (onStack == null)
+            {
+                throw new SigilException("CopyBlock expects three values to be on the stack", Stack);
+            }
+
+            var dest = onStack[2];
+            var source = onStack[1];
+            var count = onStack[0];
+
+            if (!(dest.IsPointer || dest.IsReference || dest == TypeOnStack.Get<NativeInt>()))
+            {
+                throw new SigilException("CopyBlock expects the destination value to be a pointer, reference, or native int; found " + dest, Stack);
+            }
+
+            if (!(source.IsPointer || source.IsReference || source == TypeOnStack.Get<NativeInt>()))
+            {
+                throw new SigilException("CopyBlock expects the source value to be a pointer, reference, or native int; found " + source, Stack);
+            }
+
+            if (count != TypeOnStack.Get<int>())
+            {
+                throw new SigilException("CopyBlock expects the count value to be an int; found " + count, Stack);
+            }
+
+            UpdateState(OpCodes.Cpblk, pop: 3);
+        }
+    }
+}
