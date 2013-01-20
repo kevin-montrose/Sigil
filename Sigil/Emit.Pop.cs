4e6d8f43798508c7fa65c0f11f593af46d7073ee
Kevin Montrose <kevin.montrose@stackoverflow.com>
2013-01-19 20:46:19 -0500

    Pop

---
+++ Sigil/Emit.Pop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sigil
+{
+    public partial class Emit<DelegateType>
+    {
+        public void Pop()
+        {
+            var onStack = Stack.Top();
+
+            if (onStack == null)
+            {
+                throw new SigilException("Pop expects a value on the stack, but it was empty", Stack);
+            }
+
+            UpdateState(OpCodes.Pop, pop: 1);
+        }
+    }
+}
