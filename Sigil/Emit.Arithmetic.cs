734604af0317c92e39d180b6b3aa8b9978d077c3
Kevin Montrose <kevin.montrose@stackoverflow.com>
2013-01-19 21:16:20 -0500

    Remaining arithmetic ops

--- Sigil/Emit.Arithmetic.cs
+++ Sigil/Emit.Arithmetic.cs
@@ -183,5 +183,155 @@ namespace Sigil
 
             VerifyAndDoArithmetic("UnsignedAddOverflow", OpCodes.Add_Ovf_Un, val1, val2, allowReference: true);
         }
+
+        public void Divide()
+        {
+            var args = Stack.Top(2);
+
+            if (args == null)
+            {
+                throw new SigilException("Div requires 2 arguments be on the stack", Stack);
+            }
+
+            var val2 = args[0];
+            var val1 = args[1];
+
+            VerifyAndDoArithmetic("Div", OpCodes.Div, val1, val2);
+        }
+
+        public void UnsignedDivide()
+        {
+            var args = Stack.Top(2);
+
+            if (args == null)
+            {
+                throw new SigilException("UnsignedDiv requires 2 arguments be on the stack", Stack);
+            }
+
+            var val2 = args[0];
+            var val1 = args[1];
+
+            VerifyAndDoArithmetic("UnsignedDiv", OpCodes.Div_Un, val1, val2);
+        }
+
+        public void Multiply()
+        {
+            var args = Stack.Top(2);
+
+            if (args == null)
+            {
+                throw new SigilException("Multiply requires 2 arguments be on the stack", Stack);
+            }
+
+            var val2 = args[0];
+            var val1 = args[1];
+
+            VerifyAndDoArithmetic("Multiply", OpCodes.Mul, val1, val2);
+        }
+
+        public void MultiplyOverflow()
+        {
+            var args = Stack.Top(2);
+
+            if (args == null)
+            {
+                throw new SigilException("MultiplyOverflow requires 2 arguments be on the stack", Stack);
+            }
+
+            var val2 = args[0];
+            var val1 = args[1];
+
+            VerifyAndDoArithmetic("MultiplyOverflow", OpCodes.Mul_Ovf, val1, val2);
+        }
+
+        public void UnsignedMultiplyOverflow()
+        {
+            var args = Stack.Top(2);
+
+            if (args == null)
+            {
+                throw new SigilException("UnsignedMultiplyOverflow requires 2 arguments be on the stack", Stack);
+            }
+
+            var val2 = args[0];
+            var val1 = args[1];
+
+            VerifyAndDoArithmetic("UnsignedMultiplyOverflow", OpCodes.Mul_Ovf_Un, val1, val2);
+        }
+
+        public void Remainder()
+        {
+            var args = Stack.Top(2);
+
+            if (args == null)
+            {
+                throw new SigilException("Remainder requires 2 arguments be on the stack", Stack);
+            }
+
+            var val2 = args[0];
+            var val1 = args[1];
+
+            VerifyAndDoArithmetic("Remainder", OpCodes.Rem, val1, val2);
+        }
+
+        public void UnsignedRemainder()
+        {
+            var args = Stack.Top(2);
+
+            if (args == null)
+            {
+                throw new SigilException("UnsignedRemainder requires 2 arguments be on the stack", Stack);
+            }
+
+            var val2 = args[0];
+            var val1 = args[1];
+
+            VerifyAndDoArithmetic("UnsignedRemainder", OpCodes.Rem_Un, val1, val2);
+        }
+
+        public void Subtract()
+        {
+            var args = Stack.Top(2);
+
+            if (args == null)
+            {
+                throw new SigilException("Subtract requires 2 arguments be on the stack", Stack);
+            }
+
+            var val2 = args[0];
+            var val1 = args[1];
+
+            VerifyAndDoArithmetic("Subtract", OpCodes.Sub, val1, val2);
+        }
+
+        public void SubtractOverflow()
+        {
+            var args = Stack.Top(2);
+
+            if (args == null)
+            {
+                throw new SigilException("SubtractOverflow requires 2 arguments be on the stack", Stack);
+            }
+
+            var val2 = args[0];
+            var val1 = args[1];
+
+            VerifyAndDoArithmetic("SubtractOverflow", OpCodes.Sub_Ovf, val1, val2);
+        }
+
+        public void UnsignedSubtractOverflow()
+        {
+            var args = Stack.Top(2);
+
+            if (args == null)
+            {
+                throw new SigilException("UnsignedSubtractOverflow requires 2 arguments be on the stack", Stack);
+            }
+
+            var val2 = args[0];
+            var val1 = args[1];
+
+            VerifyAndDoArithmetic("UnsignedSubtractOverflow", OpCodes.Sub_Ovf_Un, val1, val2);
+        }
     }
 }
