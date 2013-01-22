bb55be903a5d5d9c9140bb31f7ea7a7a5fe2e258
Kevin Montrose <kevin.montrose@stackoverflow.com>
2013-01-22 00:29:57 -0500

    LoadArgumentAddress

---
+++ Sigil/Emit.LoadArgumentAddress.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+using Sigil.Impl;
+
+namespace Sigil
+{
+    public partial class Emit<DelegateType>
+    {
+        public void LoadArgumentAddress(int index)
+        {
+            if (ParameterTypes.Length == 0)
+            {
+                throw new InvalidOperationException("Delegate of type " + typeof(DelegateType) + " takes no parameters");
+            }
+
+            if (index < 0 || index >= ParameterTypes.Length)
+            {
+                throw new ArgumentOutOfRangeException("index must be between 0 and " + (ParameterTypes.Length - 1) + ", inclusive");
+            }
+
+            if (index >= byte.MinValue && index <= byte.MaxValue)
+            {
+                UpdateState(OpCodes.Ldarga_S, index, TypeOnStack.Get(ParameterTypes[index].MakePointerType()));
+
+                return;
+            }
+
+            UpdateState(OpCodes.Ldarga, index, TypeOnStack.Get(ParameterTypes[index].MakePointerType()));
+        }
+    }
+}
