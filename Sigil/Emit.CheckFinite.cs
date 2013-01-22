3db548dab420fc36e8d690b8b093202a55e8ad21
Kevin Montrose <kevin.montrose@stackoverflow.com>
2013-01-22 00:08:18 -0500

    CheckFinite

---
+++ Sigil/Emit.CheckFinite.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+using Sigil.Impl;
+
+namespace Sigil
+{
+    public partial class Emit<DelegateType>
+    {
+        public void CheckFinite()
+        {
+            var onStack = Stack.Top();
+
+            if (onStack == null)
+            {
+                throw new SigilException("CheckFinite expects a value to be on the stack, but it was empty", Stack);
+            }
+
+            var val = onStack[0];
+
+            if (val != TypeOnStack.Get<float>() && val != TypeOnStack.Get<double>())
+            {
+                throw new SigilException("CheckFinite expects a floating point value, found " + val, Stack);
+            }
+
+            // ckfinite leaves the value on the stack, oddly enough
+            UpdateState(OpCodes.Ckfinite);
+        }
+    }
+}
---
+++ SigilTests/CheckFinite.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sigil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SigilTests
+{
+    [TestClass]
+    public class CheckFinite
+    {
+        [TestMethod]
+        public void Simple()
+        {
+            var e1 = Emit<Action<double>>.NewDynamicMethod("E1");
+            e1.LoadArgument(0);
+            e1.CheckFinite();
+            e1.Pop();
+            e1.Return();
+
+            var d1 = e1.CreateDelegate();
+
+            d1(123);
+
+            try
+            {
+                d1(double.PositiveInfinity);
+                Assert.Fail("Should have thrown");
+            }
+            catch (ArithmeticException e)
+            {
+                Assert.IsNotNull(e);
+            }
+        }
+    }
+}
