1dca99404401857c28257f3486731c353a5c43e6
Kevin Montrose <kevin.montrose@stackoverflow.com>
2013-01-22 00:01:19 -0500

    Break

---
+++ Sigil/Emit.Break.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sigil
+{
+    public partial class Emit<DelegateType>
+    {
+        public void Break()
+        {
+            UpdateState(OpCodes.Break);
+        }
+    }
+}
