789809152de34a693bdd8b75816944e793ba23b3
kevin-montrose <kevin.montrose@gmail.com>
2013-01-22 23:43:52 -0500

    LoadLocalAddress

---
+++ Sigil/Emit.LoadLocalAddress.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+using Sigil.Impl;
+
+namespace Sigil
+{
+    public partial class Emit<DelegateType>
+    {
+        public void LoadLocalAddress(Local local)
+        {
+            if (local == null)
+            {
+                throw new ArgumentNullException("local");
+            }
+
+            if (local.Owner != this)
+            {
+                throw new ArgumentException("local is not owned by this Emit, and thus cannot be used");
+            }
+
+            UnusedLocals.Remove(local);
+
+            var type = local.StackType.Type;
+            if (local.StackType.IsPointer) type = type.MakePointerType();
+            if (local.StackType.IsReference) type = type.MakeByRefType();
+
+            var ptrType = type.MakePointerType();
+
+            if (local.Index >= byte.MinValue && local.Index <= byte.MaxValue)
+            {
+                UpdateState(OpCodes.Ldloca_S, local.Index, TypeOnStack.Get(ptrType));
+                return;
+            }
+
+            UpdateState(OpCodes.Ldloca, local.Index, TypeOnStack.Get(ptrType));
+        }
+    }
+}
---
+++ SigilTests/LoadlLocalAddress.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sigil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SigilTests
+{
+    [TestClass]
+    public class LoadlLocalAddress
+    {
+        [TestMethod]
+        public void Simple()
+        {
+            var e1 = Emit<Func<int>>.NewDynamicMethod("E1");
+            var a = e1.CreateLocal<int>("a");
+            e1.LoadConstant(123);
+            e1.StoreLocal(a);
+            e1.LoadLocalAddress(a);
+            e1.LoadIndirect<int>();
+            e1.Return();
+
+            var d1 = e1.CreateDelegate();
+
+            Assert.AreEqual(123, d1());
+        }
+    }
+}
