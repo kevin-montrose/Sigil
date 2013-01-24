7e2ab4f760fe42a7b994998a036566672fed28d2
kevin-montrose <kevin.montrose@gmail.com>
2013-01-23 23:58:31 -0500

    StoreObject

---
+++ Sigil/Emit.StoreObject.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+using Sigil.Impl;
+
+namespace Sigil
+{
+    public partial class Emit<DelegateType>
+    {
+        public void StoreObject<ValueType>()
+        {
+            StoreObject(typeof(ValueType));
+        }
+
+        public void StoreObject(Type valueType)
+        {
+            if (valueType == null)
+            {
+                throw new ArgumentNullException("valueType");
+            }
+
+            if (!valueType.IsValueType)
+            {
+                throw new ArgumentException("valueType must be a ValueType");
+            }
+
+            var onStack = Stack.Top(2);
+
+            if (onStack == null)
+            {
+                throw new SigilException("StoreObject expects two values on the stack", Stack);
+            }
+
+            var val = onStack[0];
+            var addr = onStack[1];
+
+            if (TypeOnStack.Get(valueType) != val)
+            {
+                throw new SigilException("StoreObject expected a " + valueType + " to be on the stack, found " + val, Stack);
+            }
+
+            if (!addr.IsPointer && !addr.IsReference && addr != TypeOnStack.Get<NativeInt>())
+            {
+                throw new SigilException("StoreObject expected a reference, pointer, or native int; found " + addr, Stack);
+            }
+
+            UpdateState(OpCodes.Stobj, pop: 2);
+        }
+    }
+}
