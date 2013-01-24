17961e4be029dd1a03a37a8c596822f3e383f5dd
kevin-montrose <kevin.montrose@gmail.com>
2013-01-23 23:42:00 -0500

    ReThrow

---
+++ Sigil/Emit.ReThrow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sigil
+{
+    public partial class Emit<DelegateType>
+    {
+        public void ReThrow()
+        {
+            if(!CatchBlocks.Any(c => c.Value.Item2 == -1))
+            {
+                throw new SigilException("ReThrow is only legal in a catch block", Stack);
+            }
+
+            UpdateState(OpCodes.Rethrow);
+        }
+    }
+}
