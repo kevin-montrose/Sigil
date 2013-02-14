653d78ee9053c6a14429448de16bbef506e0d10b
kevin-montrose <kevin.montrose@gmail.com>
2013-02-13 22:02:12 -0500

    1.2.2

--- Sigil/Properties/AssemblyInfo.cs
+++ Sigil/Properties/AssemblyInfo.cs
@@ -9,5 +9,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("f42dd257-7ff8-4710-a762-148b0f4c4550")]
 
-[assembly: AssemblyVersion("1.2.1")]
-[assembly: AssemblyFileVersion("1.2.1")]
+[assembly: AssemblyVersion("1.2.2")]
+[assembly: AssemblyFileVersion("1.2.2")]
