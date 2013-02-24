1dae7a69225d3a141f409d5c020919ac58d28854
kevin-montrose <kevin.montrose@gmail.com>
2013-02-24 01:54:58 -0500

    restoring castclass eliding tests

--- SigilTests/CastClass.cs
+++ SigilTests/CastClass.cs
@@ -56,14 +56,14 @@ namespace SigilTests
             Assert.IsFalse(instrs.Contains("castclass"));
         }
 
-        /*[TestMethod]
+        [TestMethod]
         public void ElideManyBranched()
         {
             var e1 = Emit<Func<string, string>>.NewDynamicMethod();
 
             e1.LoadArgument(0);
 
-            for (var i = 0; i < 100; i++)
+            for (var i = 0; i < 5; i++)
             {
                 var l1 = e1.DefineLabel();
                 var l2 = e1.DefineLabel();
@@ -89,7 +89,42 @@ namespace SigilTests
 
             Assert.AreEqual("foo", d1("foo"));
             Assert.IsFalse(instrs.Contains("castclass"));
-        }*/
+        }
+
+        [TestMethod]
+        public void ManyBranched()
+        {
+            var e1 = Emit<Func<object, string>>.NewDynamicMethod();
+
+            e1.LoadArgument(0);
+
+            for (var i = 0; i < 5; i++)
+            {
+                var l1 = e1.DefineLabel();
+                var l2 = e1.DefineLabel();
+                var l3 = e1.DefineLabel();
+                e1.Branch(l1);
+
+                e1.MarkLabel(l2);
+                e1.Duplicate();
+                e1.Pop();
+                e1.Branch(l3);
+
+                e1.MarkLabel(l1);
+                e1.Branch(l2);
+
+                e1.MarkLabel(l3);
+            }
+
+            e1.CastClass<string>();
+            e1.Return();
+
+            string instrs;
+            var d1 = e1.CreateDelegate(out instrs);
+
+            Assert.AreEqual("foo", d1("foo"));
+            Assert.IsTrue(instrs.Contains("castclass"));
+        }
 
         [TestMethod]
         public void Simple()
