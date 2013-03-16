e2f0c7b741c1eca8942fe7a1a1edd0a1e3640c3c
kevin-montrose <kevin.montrose@gmail.com>
2013-03-16 17:42:40 -0400

    RefValAny test

--- SigilTests/TypedReferences.cs
+++ SigilTests/TypedReferences.cs
@@ -12,20 +12,20 @@ namespace SigilTests
     [TestClass]
     public class TypedReferences
     {
-        public static int _Simple(TypedReference a)
+        public static int _MakeRef(TypedReference a)
         {
             return __refvalue( a, int?) ?? 314159;
         }
 
         [TestMethod]
-        public void Simple()
+        public void MakeRef()
         {
             var e1 = Emit<Func<int?, int>>.NewDynamicMethod();
 
             e1.LoadArgumentAddress(0);
             e1.MakeReferenceAny<int?>();
 
-            e1.Call(typeof(TypedReferences).GetMethod("_Simple", BindingFlags.Static | BindingFlags.Public));
+            e1.Call(typeof(TypedReferences).GetMethod("_MakeRef", BindingFlags.Static | BindingFlags.Public));
             e1.Return();
 
             string instrs;
@@ -36,6 +36,28 @@ namespace SigilTests
 
             Assert.AreEqual(123, a);
             Assert.AreEqual(314159, b);
+        }
+
+        [TestMethod]
+        public void RefValue()
+        {
+            var e1 = Emit<Func<int>>.NewDynamicMethod();
+            var a = e1.DeclareLocal<int?>("a");
+            e1.LoadConstant(123);
+            e1.NewObject<int?, int>();
+            e1.StoreLocal(a);
+            e1.LoadLocalAddress(a);
+            e1.MakeReferenceAny<int?>();
+            e1.ReferenceAnyValue<int?>();
+            e1.Call(typeof(int?).GetProperty("Value").GetGetMethod());
+            e1.Return();
+
+            string instrs;
+            var d1 = e1.CreateDelegate(out instrs);
+
+            var x = d1();
+
+            Assert.AreEqual(123, x);
         }
     }
 }
