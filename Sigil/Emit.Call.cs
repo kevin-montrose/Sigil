b8cd2d07107db2d91a53006add4b73fd8ce7bae8
kevin-montrose <kevin.montrose@gmail.com>
2013-09-15 23:53:44 -0400

    starting on some improvements when building methods on partially constructed types

--- Sigil/Emit.Call.cs
+++ Sigil/Emit.Call.cs
@@ -51,6 +51,88 @@ namespace Sigil
         }
 
         /// <summary>
+        /// Calls the method being constructed by the given emit.  Emits so used must have been constructed with BuildMethod or related methods.
+        /// 
+        /// Pops its arguments in reverse order (left-most deepest in the stack), and pushes the return value if it is non-void.
+        /// 
+        /// If the given method is an instance method, the `this` reference should appear before any parameters.
+        /// 
+        /// Call does not respect overrides, the implementation defined by the given MethodInfo is what will be called at runtime.
+        /// 
+        /// To call overrides of instance methods, use CallVirtual.
+        /// 
+        /// When calling VarArgs methods, arglist should be set to the types of the extra parameters to be passed.
+        /// </summary>
+        public Emit<DelegateType> Call<MethodEmit>(Emit<MethodEmit> emit, Type[] arglist = null)
+        {
+            if (emit == null)
+            {
+                throw new ArgumentNullException("emit");
+            }
+
+            if (emit.MtdBuilder == null)
+            {
+                throw new InvalidOperationException("emit must be building a method");
+            }
+
+            if (HasFlag(emit.CallingConventions, CallingConventions.VarArgs) && !HasFlag(emit.CallingConventions, CallingConventions.Standard))
+            {
+                if (arglist == null)
+                {
+                    throw new InvalidOperationException("When calling a VarArgs method, arglist must be set");
+                }
+            }
+
+            var expectedParams = ((LinqArray<Type>)emit.ParameterTypes).Select(s => TypeOnStack.Get(s)).ToList();
+
+            if (arglist != null)
+            {
+                expectedParams.AddRange(((LinqArray<Type>)arglist).Select(t => TypeOnStack.Get(t)));
+            }
+
+            // Instance methods expect this to preceed parameters
+            if (HasFlag(emit.CallingConventions, CallingConventions.HasThis))
+            {
+                var declaring = emit.MtdBuilder.DeclaringType;
+
+                if (declaring.IsValueType)
+                {
+                    declaring = declaring.MakePointerType();
+                }
+
+                expectedParams.Insert(0, TypeOnStack.Get(declaring));
+            }
+
+            var resultType = emit.ReturnType == TypeOnStack.Get(typeof(void)) ? null : emit.ReturnType;
+
+            var firstParamIsThis =
+                HasFlag(emit.CallingConventions, CallingConventions.HasThis) ||
+                HasFlag(emit.CallingConventions, CallingConventions.ExplicitThis);
+
+            IEnumerable<StackTransition> transitions;
+            if (resultType != null)
+            {
+                transitions =
+                    new[]
+                    {
+                        new StackTransition(expectedParams.Reverse().AsEnumerable(), new [] { resultType })
+                    };
+            }
+            else
+            {
+                transitions =
+                    new[]
+                    {
+                        new StackTransition(expectedParams.Reverse().AsEnumerable(), new TypeOnStack[0])
+                    };
+            }
+
+            UpdateState(OpCodes.Call, emit.MtdBuilder, Wrap(transitions, "Call"), firstParamIsThis: firstParamIsThis, arglist: arglist);
+
+            return this;
+        }
+
+        /// <summary>
         /// Calls the given method.  Pops its arguments in reverse order (left-most deepest in the stack), and pushes the return value if it is non-void.
         /// 
         /// If the given method is an instance method, the `this` reference should appear before any parameters.
--- SigilTests/Methods.cs
+++ SigilTests/Methods.cs
@@ -59,5 +59,50 @@ namespace SigilTests
             var res = (string)del.Invoke(inst, new object[] { 123 });
             Assert.AreEqual("124", res);
         }
+
+        [TestMethod]
+        public void Recursive()
+        {
+            var asm = AssemblyBuilder.DefineDynamicAssembly(new AssemblyName("Foo"), AssemblyBuilderAccess.Run);
+            var mod = asm.DefineDynamicModule("Bar");
+            var t = mod.DefineType("T");
+
+            var e1 = Emit<Func<int, int>>.BuildStaticMethod(t, "Recursive", MethodAttributes.Public);
+            var cont = e1.DefineLabel("continue");
+
+            e1.LoadArgument(0);
+            e1.LoadConstant(0);
+            e1.UnsignedBranchIfNotEqual(cont);
+
+            e1.LoadConstant(1);
+            e1.Return();
+
+            e1.MarkLabel(cont);
+            e1.LoadArgument(0);
+            e1.Duplicate();
+            e1.LoadConstant(-1);
+            e1.Add();
+            e1.Call(e1);
+            e1.Multiply();
+
+            e1.Return();
+
+            e1.CreateMethod();
+
+            var type = t.CreateType();
+            var recur = type.GetMethod("Recursive", BindingFlags.Public | BindingFlags.Static);
+
+            var zero = (int)recur.Invoke(null, new object[] { 0 });
+            var one = (int)recur.Invoke(null, new object[] { 1 });
+            var two = (int)recur.Invoke(null, new object[] { 2 });
+            var three = (int)recur.Invoke(null, new object[] { 3 });
+            var ten = (int)recur.Invoke(null, new object[] { 10 });
+
+            Assert.AreEqual(1, zero);
+            Assert.AreEqual(1 * 1, one);
+            Assert.AreEqual(2 * 1 * 1, two);
+            Assert.AreEqual(3 * 2 * 1 * 1, three);
+            Assert.AreEqual(10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1 * 1, ten);
+        }
     }
 }
