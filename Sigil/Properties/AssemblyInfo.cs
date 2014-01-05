306baf5d5a7efd9c8af2f290d0238a194d3f2502
kevin-montrose <kevin.montrose@gmail.com>
2014-01-04 21:03:51 -0500

    4.0.2

--- Sigil/Properties/AssemblyInfo.cs
+++ Sigil/Properties/AssemblyInfo.cs
@@ -4,10 +4,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Sigil")]
 [assembly: AssemblyDescription("A fail-fast validating helper for .NET CIL generation")]
 [assembly: AssemblyProduct("Sigil")]
-[assembly: AssemblyCopyright("Copyright © Kevin Montrose 2013")]
+[assembly: AssemblyCopyright("Copyright © Kevin Montrose 2013-2014")]
 
 [assembly: ComVisible(false)]
 [assembly: Guid("f42dd257-7ff8-4710-a762-148b0f4c4550")]
 
-[assembly: AssemblyVersion("4.0.1")]
-[assembly: AssemblyFileVersion("4.0.1")]
+[assembly: AssemblyVersion("4.0.2")]
+[assembly: AssemblyFileVersion("4.0.2")]
